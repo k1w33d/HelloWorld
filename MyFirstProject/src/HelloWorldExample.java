c0c068ea9b6edfa12865b83c0437fdfa91ca47d9
k1w33d <k1w33d@gmail.com>
2020-11-13 11:21:50 +1300

    Add comment

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
 
